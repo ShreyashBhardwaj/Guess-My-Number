4e92bd1178a0adb41d4648b0e2b22eeb6d0b63b8
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-10-03 15:59:11 +0530

    Added the HighScore Functionality

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 "use strict";
-let score = document.querySelector(".score");
+let scoreGUI = document.querySelector(".score");
+let score = 20;
 let highScore = document.querySelector(".highscore");
-console.log(highScore);
-
-const randomValue = Math.floor(Math.random() * (20 - 1)) + 1;
+let currentHighScore = Number(document.querySelector(".highscore").innerHTML);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
 const actualValue = document.querySelector(".number");
 
-console.log(randomValue);
+console.log(secretNumber);
 const checkButton = document.querySelector(".check");
 const message = document.querySelector(".message");
 
 function checkValue(playerGuess) {
-  if (playerGuess === randomValue) {
+  if (playerGuess === secretNumber) {
     message.innerHTML = "You did it great 🎉";
-    highScore.innerHTML = score;
+    highScore.innerHTML = scoreGUI;
     document.querySelector("body").style.backgroundColor = "#60b347";
-    actualValue.innerHTML = randomValue;
+    actualValue.innerHTML = secretNumber;
+
+    if (score > currentHighScore) {
+      highScore.innerHTML = score;
+    }
   } else {
-    if (playerGuess > randomValue) {
+    if (playerGuess > secretNumber) {
       message.innerHTML = "Too High 😥";
     } else {
       message.innerHTML = "Too Low 😥";
     }
-    score.innerHTML = score.innerHTML - 1;
+    score -= 1;
+    scoreGUI.innerHTML = score;
   }
 }
 
 checkButton.addEventListener("click", function () {
   let playerGuess = Number(document.querySelector(".guess").value);
-  checkValue(playerGuess);
+  if (!playerGuess) {
+    message.innerHTML = "Enter a Number Please!";
+  } else {
+    checkValue(playerGuess);
+  }
 });
